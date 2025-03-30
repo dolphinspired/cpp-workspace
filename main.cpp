834bb2eb537d4e3c60dbef7bcd57be14a894948a
dolphinspired <ultima749@gmail.com>
2025-03-30 09:54:28 -0400

    Formatting

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     std::cout << "Hello World" << std::endl;
 
-    std::cin.clear(); // reset any error flags
+    std::cin.clear();                                                   // reset any error flags
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
-    std::cin.get(); // get one more char from the user
+    std::cin.get();                                                     // get one more char from the user
 }
